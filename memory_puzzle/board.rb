ab2a2674e4ed71ba108137b624e4b592492d4ffd
q463250938@gmail.com <q463250938@gmail.com>
2022-10-18 16:54:43 -0700

    memory_puzle1

---
+++ memory_puzzle/board.rb
@@ -0,0 +1,74 @@
+require "./card.rb"
+class Board
+    attr_accessor :grid, :card1, :card2
+    def initialize(n=4)
+        @grid = Array.new(n){Array.new(n)}
+        populate
+        raise "pick a different number" if !(n*n).even? 
+    end
+    def [](pos)
+        @grid[pos[0]][pos[1]]
+    end
+    def []=(pos,value)
+        @grid[pos[0]][pos[1]] = value
+    end
+    def empty_spots?
+        (0...@grid.length).each do |i|
+            return true if @grid[i].any? {|ele| ele == nil}
+        end
+        false
+    end    
+    def populate
+        #create two instances of card      
+        # gets_position = (0..grid.length).all? do ||
+        
+        while self.empty_spots?
+            arr =[]
+            chars = []
+            rc = ("a".."z").to_a.sample(1).join("")
+            card1 = Card.new(rc)
+            card2 = Card.new(card1.mark)
+            random_index_1 = rand(0...@grid.length)
+            random_index_2 = rand(0...@grid.length)
+            random_index_3 = rand(0...@grid.length)
+            random_index_4 = rand(0...@grid.length)
+
+
+           if self[[random_index_1,random_index_2]] == nil && self[[random_index_3,random_index_4]] == nil && !arr.include?(card1.mark) && !arr.include?(card2.mark)
+            self[[random_index_3,random_index_4]] = card2
+            self[[random_index_1,random_index_2]] = card1
+            arr << card1.mark
+           end
+        end
+    end
+
+    def render
+        output= []
+        (0...@grid.length).each do |i|
+            subarr = []
+            (0...@grid[i].length).each do |j|
+                if @grid[i][j].face_up == true
+                    subarr << grid[i].mark
+                     subarr
+                else
+                    subarr <<  "_"
+                end
+               
+            end
+         p subarr.join(" ")
+        end
+       
+    end
+    # def render_help(arr)
+    #     arr.map do |card|
+    #         if card.face_up == true
+    #             puts card.mark
+    #         else
+    #             puts " "
+    #         end
+    #     end
+    #     arr
+    # end
+
+
+end
---
+++ memory_puzzle/card.rb
@@ -0,0 +1,30 @@
+class Card
+    attr_accessor :face_up, :face_down, :mark
+
+    def initialize(mark)
+        @face_up = false
+        @face_down = true
+        @mark = mark
+    end
+
+    def reveal(mark)
+        @face_down = false
+        @face_up = true
+        p @mark
+    end
+
+    def hide(mark)
+        @face_down = true
+        @face_up = false
+        p " "
+    end
+
+    # def to_s(mark)
+    #     mark.to_s
+    # end
+
+    def ==(mark)
+        self.mark == mark
+    end
+
+end
